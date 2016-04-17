7ce00cbbd186c2413ff5fb68618d4e25cdad8cd5
Chen Huang <alan.tolearn@gmail.com>
2016-04-17 14:29:50 -0400

    Create migration to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417052157) do
+ActiveRecord::Schema.define(version: 20160417181935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "title_number",   null: false
+    t.string   "title_unit"
+    t.string   "subtitle"
+    t.datetime "date_published", null: false
+    t.text     "article",        null: false
+    t.string   "source"
+    t.integer  "views"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
