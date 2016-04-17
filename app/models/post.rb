b87eff9db6bcea6ddb5d723ad088210983c77bf1
Chen Huang <alan.tolearn@gmail.com>
2016-04-17 14:49:44 -0400

    Add post model
    - add validators
    - add init values

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+  validates :title_number, presence: true
+  validates :date_published, presence: true
+  validates :article, presence: true
+
+  after_initialize :init
+
+  def init
+    self.date_published ||= Time.now
+    self.views ||= 0
+  end
+end
